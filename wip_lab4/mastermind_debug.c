8dc9cdfb3f11134cb3244763b83f6bce4f0c0c09
Jakub <ogrodowczykjakub@gmail.com>
2022-01-19 16:15:59 +0100

    Zrobienie zadania 2 z listy 4 mastermind i updated README

--- wip_lab4/mastermind_debug.c
+++ wip_lab4/mastermind_debug.c
@@ -7,6 +7,8 @@
 
 
 int rounds_num;
+FILE* out;
+
 
 void guesses_init(int* values, bool* bitmask)
 {
@@ -30,6 +32,51 @@ void guesses_init(int* values, bool* bitmask)
     }
     for(int i = 0; i < 6 * 6 * 6 * 6; i++)
         bitmask[i] = true;
+}
+
+
+void flush_guesses(int* kombinacje, bool* bitmask, bool hide)
+{
+    for(int i = 0; i < 4 * 6 * 6 * 6 * 6; i+=4)
+    {
+        if(hide)
+        {
+            if(bitmask[i/4] == true)
+            {
+                printf("i = %d: [%d] [%d] [%d] [%d]\n",
+                i/4, kombinacje[i + 0], kombinacje[i + 1],
+                kombinacje[i + 2], kombinacje[i + 3]);
+                fprintf(out, "i = %d: [%d] [%d] [%d] [%d]\n",
+                i/4, kombinacje[i + 0], kombinacje[i + 1],
+                kombinacje[i + 2], kombinacje[i + 3]);
+            }
+        } 
+        else
+        {
+            fprintf(out, "i = %d: %d [%d] [%d] [%d] [%d]\n",
+            i/4, bitmask[i/4], kombinacje[i + 0], kombinacje[i + 1],
+            kombinacje[i + 2], kombinacje[i + 3]);
+        }
+            
+    }
+}
+
+
+void print_guess_debug(int* kombinacje, int* prev_guess, 
+int red, int white, int amount_red, int amount_white, int i)
+{
+    fprintf(out, "========================================\n");
+    fprintf(out, "prev guess:\n");
+    fprintf(out, "[%d] [%d] [%d] [%d]\n", prev_guess[0], prev_guess[1],
+    prev_guess[2], prev_guess[3]);
+    fprintf(out, "curr guess:\n");
+    fprintf(out, "[%d] [%d] [%d] [%d]\n", kombinacje[i + 0], kombinacje[i + 1],
+    kombinacje[i + 2], kombinacje[i + 3]);
+    fprintf(out, "ilosc czerwonych = %d\n",amount_red);
+    fprintf(out, "red: (%d == %d)\n",red,amount_red);
+    fprintf(out, "ilosc bialych = %d\n",amount_white);
+    fprintf(out, "white: (%d == %d)\n",white,amount_white);
+    fprintf(out, "%s\n", ((amount_white == white && amount_red == red) ? "valid" : "invalid"));
 }
 
 
@@ -58,7 +105,9 @@ void guess(int* kombinacje, bool* bitmask, int* prev_guess, int red, int white)
                     break;
                 }
             }
-        }        
+        }
+        print_guess_debug(kombinacje,prev_guess,red,white,amount_red,amount_white,i);
+        
 
         bitmask[i / 4] = (amount_white == white && amount_red == red);
     }
@@ -69,6 +118,8 @@ bool guess_and_print(int* kombinacje, bool* bitmask, int* prev_guess, int red, i
 {
     // to do
     guess(kombinacje, bitmask, prev_guess, red, white);
+
+    flush_guesses(kombinacje, bitmask, true);
 
     // wypisujemy pierwsza mozliwa permutajce
     for(int i = 0; i < 4 * 6 * 6 * 6 * 6; i += 4)
@@ -92,6 +143,8 @@ bool guess_and_print(int* kombinacje, bool* bitmask, int* prev_guess, int red, i
 
 int main()
 {
+    out = fopen("out.txt","w");
+
     int* kombinacje = (int*)malloc(4 * sizeof(int) * 6 * 6 * 6 * 6);
     bool* bitmask = (bool*)malloc(sizeof(bool) * 6 * 6 * 6 * 6);
 
@@ -124,6 +177,7 @@ int main()
         rounds_num++;
     }
 
+    fclose(out);
     free(kombinacje);
     free(bitmask);
     free(prev_guess);
