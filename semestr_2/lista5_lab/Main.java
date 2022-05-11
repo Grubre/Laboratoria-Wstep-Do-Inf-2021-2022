596685554d10314d601ff6d4f6d8a9fc2ae6a34d
Jakub <ogrodowczykjakub@gmail.com>
2022-05-11 17:20:48 +0200

    dodana lista 5

---
+++ semestr_2/lista5_lab/Main.java
@@ -0,0 +1,32 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.ToolBar;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+    public static int AppWidth  = 800;
+    public static int AppHeight = 500;
+    public static Stage mStage;
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        mStage = stage;
+
+        AnchorPane root = new AnchorPane();
+        MainScene.ConstructScene(root);
+
+        Scene scene = new Scene(root, AppWidth, AppHeight);
+
+        mStage.setTitle("Edytor Ksztaltow");
+        mStage.setScene(scene);
+        mStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ semestr_2/lista5_lab/PropertyAdder.java
@@ -0,0 +1,53 @@
+import javafx.event.EventHandler;
+import javafx.scene.Node;
+import javafx.scene.input.MouseEvent;
+/**
+ * Class that adds the dragging and rotation properties to an object
+ */
+public class PropertyAdder {
+    private final Node target;
+    private double anchorRotate;
+    private double initialX;
+    private double initialY;
+    private EventHandler<MouseEvent> setAnchor;
+    private EventHandler<MouseEvent> updatePositionOnDrag;
+    private int cycleStatus = 0;
+    /**
+     * Constructor in which we specify the target, create the handlers and add event filters
+     */
+    public PropertyAdder(Node target) {
+        this.target = target;
+        createHandlers();
+        target.addEventFilter(MouseEvent.MOUSE_PRESSED, setAnchor);
+        target.addEventFilter(MouseEvent.MOUSE_DRAGGED, updatePositionOnDrag);
+    }
+    /**
+     * Method that creates event handlers for mouse dragging and rotating
+     */
+    private void createHandlers() {
+        setAnchor = event -> {
+            if (event.isPrimaryButtonDown()) {
+                cycleStatus = 1;
+                initialX = event.getSceneX();
+                initialY = event.getSceneY();
+            }
+            if (event.isMiddleButtonDown()) {
+                cycleStatus = 0;
+                anchorRotate = target.getRotate();
+            }
+            if(event.isSecondaryButtonDown())
+                cycleStatus = 0;
+        };
+        updatePositionOnDrag = event -> {
+            if (cycleStatus != 0) {
+                target.setTranslateX(target.getTranslateX() + event.getSceneX() - initialX);
+                target.setTranslateY(target.getTranslateY() + event.getSceneY() - initialY);
+                initialX = event.getSceneX();
+                initialY = event.getSceneY();
+            }
+            if(event.isMiddleButtonDown())
+                target.setRotate(event.getSceneX() - initialX + anchorRotate);
+        };
+    }
+
+}
