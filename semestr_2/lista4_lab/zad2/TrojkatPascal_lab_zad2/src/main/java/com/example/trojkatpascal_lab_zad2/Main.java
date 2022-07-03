519e70313a4cfaf80426e18b2ea6d4f2cc9816aa
Grubre <ogrodowczykjakub@gmail.com>
2022-07-03 23:18:59 +0200

    Usunięta reszta dokumentacji oraz foldery z intellij

--- semestr_2/lista4_lab/zad2/TrojkatPascal_lab_zad2/src/main/java/com/example/trojkatpascal_lab_zad2/Main.java
+++
@@ -1,7 +0,0 @@
-package com.example.trojkatpascal_lab_zad2;
-
-public class Main {
-    public static void main(String[] args) {
-        TrojkatPascala.main(args);
-    }
-}
--- semestr_2/lista5_lab/AutoCadv2/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.autocadv2 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.autocadv2 to javafx.fxml;
-    exports com.example.autocadv2;
-}
