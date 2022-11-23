f03c31971bbccf9bb8c0a139e51455293c16dcd7
Grubre <ogrodowczykjakub@gmail.com>
2022-11-23 19:44:01 +0100

    Finished exercise 3 from list4 akiso

---
+++ semester3/akiso/list4/exercise3.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+chuck=$(curl https://api.chucknorris.io/jokes/random --silent | jq '.value')
+
+cat=$(curl https://api.thecatapi.com/v1/images/search  --silent )
+url=$((jq '.[0].url' <<< $cat) | tr -d '"' )
+width=$(jq '.[0].width' <<< $cat)
+height=$(jq '.[0].height' <<< $cat)
+
+w_terminal=$( expr $(tput cols) / 2 )
+h_terminal=$( expr $(tput lines) / 2 )
+
+curl $url  --silent  > newcat.jpg
+
+img2txt newcat.jpg  -f utf8  
+echo $chuck
