62f22cb2c62fe79a7f43138c9f5d706215f5f3e8
Grubre <ogrodowczykjakub@gmail.com>
2022-12-01 23:09:31 +0100

    Progress in exercises 1 and 2

--- semester3/akiso/list4/exercise1.sh
+++ semester3/akiso/list4/exercise1.sh
@@ -5,21 +5,28 @@ do
     PID=${proc:6}
     STATE_CODE=`expr 52 - 3`
     PPID_CODE=`expr 52 - 4`
-    TTY_CODE=`expr 52 - 7`
     RSS_CODE=`expr 52 - 24`
     SID_CODE=`expr 52 - 6`
     PGID_CODE=`expr 52 - 5`
-    CODES=($STATE_CODE $PPID_CODE $TTY_CODE $RSS_CODE $SID_CODE $PGID_CODE)
-    COMM=$(cat $proc/comm)
-    
-    output+=$PID$"\t"
-    output+=$COMM$"\t"
-    for code in ${CODES[@]}
-    do
-        output+=$(awk '{ print $(NF-'$code') } ' $proc/stat 2>/dev/null)$"\t"
-    done
+    CODES=($STATE_CODE $PPID_CODE $RSS_CODE $SID_CODE $PGID_CODE)
+    COMM=$(cat $proc/comm 2>/dev/null)
+    TTY=$(readlink $proc/fd/1 2>/dev/null)
+    if [ -z $TTY ]
+    then
+        TTY="?"
+    fi
+        
+    if [[ ! -z "$COMM" ]]
+    then
+        output+=$PID$"\t"
+        output+=$COMM$"\t"
+        for code in ${CODES[@]}
+        do
+            output+=$(awk '{ print $(NF-'$code') } ' $proc/stat 2>/dev/null)$"\t"
+        done
+        output+=$TTY
+        output+='\n'
+    fi
 
-    output+='\n'
 done
-echo -e $output | column -s $'\t' --table --table-columns PID,COMM,STATE,PPID,TTY,RSS,SID,PGID 
-# echo -e $output 
+echo -e $output | column -s $'\t' --table --table-columns PID,COMM,STATE,PPID,RSS,SID,PGID,TTY
--- semester3/akiso/list4/exercise2.sh
+++ semester3/akiso/list4/exercise2.sh
@@ -1,36 +1,132 @@
 # cat /proc/net/dev
-print_transfer () {
+# ==================================================================================================
+# GET TRANSFER
+# ==================================================================================================
+convert_transfer () {
+    read in echo $in
     thousand=1000
     million=1000000
     billion=1000000000
-    if [[ $1 -lt $thousand ]]; then
-        echo $1" b"
-    elif [[ $1 -lt $million ]]; then
-        val=`expr $1 / 1000`
+    if [[ $in -lt $thousand ]]; then
+        echo $in" b"
+    elif [[ $in -lt $million ]]; then
+        val=`expr $in / 1000`
         echo $val" kb"
-    elif [[ $1 -lt $billion ]]; then
-        val=`expr $1 / 1000000`
+    elif [[ $in -lt $billion ]]; then
+        val=`expr $in / 1000000`
         echo $val" mb"
-
     fi
 }
-
-# GET TRANSFER
-
 RECEIVE_TRANSFER_COL=2
 TRANSMITTED_TRANSFER_COL=10
-echo -n "Received transfer = "
-print_transfer $(awk 'NR>2{print $'$RECEIVE_TRANSFER_COL'}' /proc/net/dev | awk '{s+=$1}END{print s}')
-echo -n "Sent transfer = "
-print_transfer $(awk 'NR>2{print $'$TRANSMITTED_TRANSFER_COL'}' /proc/net/dev | awk '{s+=$1}END{print s}')
+print_received_transfer () {
+    echo $(awk 'NR>2{print $'$RECEIVE_TRANSFER_COL'}' /proc/net/dev | awk '{s+=$1}END{print s}')
+}
+print_sent_transfer () {
+    echo $(awk 'NR>2{print $'$TRANSMITTED_TRANSFER_COL'}' /proc/net/dev | awk '{s+=$1}END{print s}')
+}
+SENT_VALS=(0 0 0 0 0 0 0 0 0 0)
+RECEIVED_VALS=(0 0 0 0 0 0 0 0 0 0)
+DIAG_WIDTH=2
+DIAG_HEIGHT=10
+move_bars () {
+    for ((sent_iter = ${#SENT_VALS[@]}-1; sent_iter > 0; sent_iter--)); do
+        SENT_VALS[$sent_iter]=${SENT_VALS[$sent_iter - 1]}
+        RECEIVED_VALS[$sent_iter]=${RECEIVED_VALS[$sent_iter - 1]}
+    done
+    RECEIVED_VALS=$(expr $(print_received_transfer) - $prev_received_transfer | cat)
+    SENT_VALS=$(expr $(print_sent_transfer) - $prev_sent_transfer | cat)
+}
+repeat_char () {
+    for ((i = 0; i < $2; i++)); do
+        echo -n "$1"
+    done
+}
+draw_diagram () {
+    diag_in=("$@")
+    # calculate max
+    max=${diag_in[0]}
+    for n in "${diag_in[@]}" ; do
+        ((n > max)) && max=$n
+    done
+    if [[ $max -eq "0" ]]; then
+        max=1
+    fi
+    repeat_char "=" $(expr $DIAG_WIDTH "*" $DIAG_HEIGHT + 2)
+    echo
+    for ((diag_loop = $DIAG_HEIGHT; diag_loop > 0; diag_loop--)); do
+        echo -n "|"
+        for n in "${diag_in[@]}" ; do
+            # echo -n $(echo "scale=0; $DIAG_HEIGHT * $n / $max" | bc) "<" $diag_loop
+            if [ $(echo "scale=0; $DIAG_HEIGHT * $n / $max" | bc) -ge $diag_loop ]
+            then
+                repeat_char "█" $DIAG_WIDTH
+            else
+                repeat_char "." $DIAG_WIDTH
+            fi
+        done
+        echo -n "|"
+        if [[ $diag_loop -eq $DIAG_HEIGHT ]]; then
+            echo -n " $(echo $max | convert_transfer)                 "
+        fi
+        echo
+    done
+
+    repeat_char "=" $(expr $DIAG_WIDTH "*" $DIAG_HEIGHT + 2)
+    echo
+}
 
 # GET PER THREAD USAGE
-max_cpu_fq=$(cat /sys/devices/system/cpu/cpufreq/policy0/cpuinfo_max_freq)
-min_cpu_fq=$(cat /sys/devices/system/cpu/cpufreq/policy0/cpuinfo_min_freq)
-cat /proc/cpuinfo | grep "cpu MHz" | cut -d " " -f3 | {
-    for nr in $(cat)
+# IN OTHER FILE
+per_thread_info () {
+    currentDate=$(date +%s%N | cut -b1-13)
+    currentStats=$(cat /proc/stat)    
+
+    cpus=$(echo "$currentStats" | grep -P 'cpu' | awk -F " " '{print $1}')
+
+    for cpu in $cpus
     do
-        echo "($nr*100*100-$min_cpu_fq)/($max_cpu_fq)" | bc -l
+        currentLine=$(echo "$currentStats" | grep "$cpu ")
+        user=$(echo "$currentLine" | awk -F " " '{print $2}')
+        nice=$(echo "$currentLine" | awk -F " " '{print $3}')
+        system=$(echo "$currentLine" | awk -F " " '{print $4}')
+        idle=$(echo "$currentLine" | awk -F " " '{print $5}')
+        iowait=$(echo "$currentLine" | awk -F " " '{print $6}')
+        irq=$(echo "$currentLine" | awk -F " " '{print $7}')
+        softirq=$(echo "$currentLine" | awk -F " " '{print $8}')
+        steal=$(echo "$currentLine" | awk -F " " '{print $9}')
+        guest=$(echo "$currentLine" | awk -F " " '{print $10}')
+        guest_nice=$(echo "$currentLine" | awk -F " " '{print $11}')
+
+        previousLine=$(echo "$previousStats" | grep "$cpu ")
+        prevuser=$(echo "$previousLine" | awk -F " " '{print $2}')
+        prevnice=$(echo "$previousLine" | awk -F " " '{print $3}')
+        prevsystem=$(echo "$previousLine" | awk -F " " '{print $4}')
+        previdle=$(echo "$previousLine" | awk -F " " '{print $5}')
+        previowait=$(echo "$previousLine" | awk -F " " '{print $6}')
+        previrq=$(echo "$previousLine" | awk -F " " '{print $7}')
+        prevsoftirq=$(echo "$previousLine" | awk -F " " '{print $8}')
+        prevsteal=$(echo "$previousLine" | awk -F " " '{print $9}')
+        prevguest=$(echo "$previousLine" | awk -F " " '{print $10}')
+        prevguest_nice=$(echo "$previousLine" | awk -F " " '{print $11}')    
+
+        PrevIdle=$((previdle + previowait))
+        Idle=$((idle + iowait))
+
+        PrevNonIdle=$((prevuser + prevnice + prevsystem + previrq + prevsoftirq + prevsteal))
+        NonIdle=$((user + nice + system + irq + softirq + steal))
+
+        PrevTotal=$((PrevIdle + PrevNonIdle))
+        Total=$((Idle + NonIdle))
+
+        totald=$((Total - PrevTotal))
+        idled=$((Idle - PrevIdle))
+
+        CPU_Percentage=$(awk "BEGIN {print ($totald - $idled)/$totald*100}")
+
+        if [[ "$cpu" != "cpu" ]]; then
+            printf "$cpu %.2f%%        \n" "$CPU_Percentage"
+        fi
     done
 }
 
@@ -38,7 +134,29 @@ cat /proc/cpuinfo | grep "cpu MHz" | cut -d " " -f3 | {
 uptime=$(cat /proc/uptime | cut -d ' ' -f1)
 echo "uptime = "$uptime
 
-# while [[ true ]]; do
-#     echo "abc"
-#     sleep 1
-# done
+prev_received_transfer=$(expr $(print_received_transfer))
+prev_sent_transfer=$(expr $(print_sent_transfer))
+
+tput civis
+tput clear
+while [[ true ]]; do
+    output=""
+    previousDate=$(date +%s%N | cut -b1-13)
+    previousStats=$(cat /proc/stat)
+
+    sleep 1
+
+    tput cup 0 0
+    # PRINT TRANSFER
+    move_bars
+    output+="RECEIVED"$"\n"
+    output+=$(draw_diagram "${RECEIVED_VALS[@]}")$"\n"
+    output+="SENT"$"\n"
+    output+=$(draw_diagram "${SENT_VALS[@]}")$"\n"
+    prev_received_transfer=$(print_received_transfer)
+    prev_sent_transfer=$(print_sent_transfer)
+
+    output+=$(per_thread_info)
+    echo -e "$output"
+    done
+tput cnorm
