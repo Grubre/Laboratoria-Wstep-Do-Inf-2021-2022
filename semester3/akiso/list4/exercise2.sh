59be33155327b912f96785074f17412450480f29
Grubre <ogrodowczykjakub@gmail.com>
2022-12-04 16:47:02 +0100

    Finished exercise 2 from list 4 akiso

--- semester3/akiso/list4/exercise2.sh
+++ semester3/akiso/list4/exercise2.sh
@@ -2,21 +2,6 @@
 # ==================================================================================================
 # GET TRANSFER
 # ==================================================================================================
-convert_transfer () {
-    read in echo $in
-    thousand=1000
-    million=1000000
-    billion=1000000000
-    if [[ $in -lt $thousand ]]; then
-        echo $in" b"
-    elif [[ $in -lt $million ]]; then
-        val=`expr $in / 1000`
-        echo $val" kb"
-    elif [[ $in -lt $billion ]]; then
-        val=`expr $in / 1000000`
-        echo $val" mb"
-    fi
-}
 RECEIVE_TRANSFER_COL=2
 TRANSMITTED_TRANSFER_COL=10
 print_received_transfer () {
@@ -27,8 +12,8 @@ print_sent_transfer () {
 }
 SENT_VALS=(0 0 0 0 0 0 0 0 0 0)
 RECEIVED_VALS=(0 0 0 0 0 0 0 0 0 0)
-DIAG_WIDTH=2
-DIAG_HEIGHT=10
+DIAG_WIDTH=5
+DIAG_HEIGHT=8
 move_bars () {
     for ((sent_iter = ${#SENT_VALS[@]}-1; sent_iter > 0; sent_iter--)); do
         SENT_VALS[$sent_iter]=${SENT_VALS[$sent_iter - 1]}
@@ -52,7 +37,7 @@ draw_diagram () {
     if [[ $max -eq "0" ]]; then
         max=1
     fi
-    repeat_char "=" $(expr $DIAG_WIDTH "*" $DIAG_HEIGHT + 2)
+    repeat_char "=" $(expr $DIAG_WIDTH "*" 10 + 2)
     echo
     for ((diag_loop = $DIAG_HEIGHT; diag_loop > 0; diag_loop--)); do
         echo -n "|"
@@ -67,12 +52,12 @@ draw_diagram () {
         done
         echo -n "|"
         if [[ $diag_loop -eq $DIAG_HEIGHT ]]; then
-            echo -n " $(echo $max | convert_transfer)                 "
+            echo -n " $(echo $max | numfmt --to=iec)                 "
         fi
         echo
     done
 
-    repeat_char "=" $(expr $DIAG_WIDTH "*" $DIAG_HEIGHT + 2)
+    repeat_char "=" $(expr $DIAG_WIDTH "*" 10 + 2)
     echo
 }
 
@@ -129,26 +114,44 @@ per_thread_info () {
         fi
     done
 }
-
 # UPTIME
-uptime=$(cat /proc/uptime | cut -d ' ' -f1)
-echo "uptime = "$uptime
-
+print_uptime () {
+    uptime=$(cat /proc/uptime | cut -d ' ' -f1)
+    echo -en "\n"
+    eval "echo Uptime = $(date -ud "@$uptime" +'$((%s/3600/24)) days %H hours %M minutes %S seconds')"
+}
+# BATTERY
+print_battery () {
+    full=$(cat /sys/class/power_supply/BAT1/uevent | grep "POWER_SUPPLY_CHARGE_FULL_DESIGN=" | cut -d '=' -f2)
+    now=$(cat /sys/class/power_supply/BAT1/uevent | grep "POWER_SUPPLY_CHARGE_NOW=" | cut -d '=' -f2)
+    echo "\nBattery = "$(echo "scale=2; 100 * $now / $full" | bc)"%"
+}
+# LOADAVG
+print_loadavg () {
+    echo "\nLoadavg = $(cat /proc/loadavg | cut -d " " -f1,2,3 --output-delimiter ', ')"
+}
+# MEMINFO
+print_memory_usage () {
+    total=$(cat /proc/meminfo | grep "MemTotal" | sed 's/  */ /g' | cut -d " " -f2)
+    free=$(cat /proc/meminfo | grep "MemFree" | sed 's/  */ /g' | cut -d " " -f2)
+    buffers=$(cat /proc/meminfo | grep "Buffers" | sed 's/  */ /g' | cut -d " " -f2)
+    cache=$(cat /proc/meminfo | grep -w "Cached" | sed 's/  */ /g' | cut -d " " -f2)
+    echo "\nMemory usage = "$(expr $total - $free - $buffers - $cache | numfmt --to=iec --from-unit=1000)
+}
 prev_received_transfer=$(expr $(print_received_transfer))
 prev_sent_transfer=$(expr $(print_sent_transfer))
 
-tput civis
 tput clear
 while [[ true ]]; do
     output=""
     previousDate=$(date +%s%N | cut -b1-13)
     previousStats=$(cat /proc/stat)
 
-    sleep 1
+    # sleep 1
 
-    tput cup 0 0
     # PRINT TRANSFER
     move_bars
+    tput cup 0 0
     output+="RECEIVED"$"\n"
     output+=$(draw_diagram "${RECEIVED_VALS[@]}")$"\n"
     output+="SENT"$"\n"
@@ -157,6 +160,9 @@ while [[ true ]]; do
     prev_sent_transfer=$(print_sent_transfer)
 
     output+=$(per_thread_info)
+    output+=$(print_uptime)
+    output+=$(print_battery)
+    output+=$(print_loadavg)
+    output+=$(print_memory_usage)
     echo -e "$output"
-    done
-tput cnorm
+done
