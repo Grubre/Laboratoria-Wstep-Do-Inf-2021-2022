f5d7460c581ae5a9d9f5eab5016812cfbf1587af
Grubre <ogrodowczykjakub@gmail.com>
2023-11-16 14:30:22 +0100

    3 first exercises on.l3

---
+++ semester5/on/l3/src/solvers.jl
@@ -0,0 +1,75 @@
+# Author: Jakub Ogrodowczyk
+
+module Solvers
+
+function mbisekcji(f::Function, a::Float64, b::Float64, delta::Float64, epsilon::Float64)
+    if sign(f(a)) == sign(f(b))
+        return Nothing, Nothing, Nothing, 1
+    end
+
+    it = 1
+    
+    while true
+        c = (a + b) / 2
+
+        f_c = f(c)
+
+        if f_c == 0 || (b - a) / 2 < delta || abs(f_c) < epsilon
+            return c, f_c, it, 0
+        end
+
+        it += 1
+        if (sign(f_c) == sign(f(a)))
+            a = c
+        else
+            b = c
+        end
+    end
+end
+
+
+function mstycznych(f::Function, pf::Function, x0::Float64, delta::Float64, epsilon::Float64, maxit::Int)
+    it = 1
+    x = x0
+    while it < maxit
+        y = f(x)
+        yp = pf(x)
+
+        if abs(yp) < epsilon
+            return x, y, it, 2
+        end
+        
+        x1 = x - y / yp
+
+        if abs(x1 - x) < delta
+            return x1, f(x1), it, 0
+        end
+
+        x = x1
+
+        it += 1
+    end
+    return x, f(x), it, 1
+end
+
+function msiecznych(f::Function, x0::Float64, x1::Float64, delta::Float64, epsilon::Float64, maxit::Int)
+    it = 1
+
+    while it < maxit
+        x2 = x1 - f(x1) * (x1 - x0) / (f(x1) - f(x0))
+
+        if abs(x2 - x1) < delta || abs(f(x2)) < epsilon
+            return x2, f(x2), it, 0
+        end
+
+        x0 = x1
+        x1 = x2
+
+        it += 1
+    end
+
+    return x1, f(x1), it, 1
+end
+
+
+end
---
+++ semester5/on/l3/test/tests.jl
@@ -0,0 +1,143 @@
+using Test
+include("../src/solvers.jl")
+using .Solvers
+
+@testset "Testy metody bisekcji" begin
+    @testset "test rownania kwadratowego" begin
+        f(x) = x^2 - 4
+        a, b = 1.0, 5.0
+        delta, epsilon = 1e-5, 1e-5
+        r, v, it, err = Solvers.mbisekcji(f, a, b, delta, epsilon)
+        
+        @test err == 0
+        @test r ≈ sqrt(4) atol=delta
+        @test abs(v) < epsilon
+        @test it > 0
+    end
+
+    @testset "test rownania trzeciego stopnia" begin
+        f(x) = x^3 - 1
+        a, b = 0.0, 2.0
+        delta, epsilon = 1e-5, 1e-5
+        r, v, it, err = Solvers.mbisekcji(f, a, b, delta, epsilon)
+
+        @test err == 0
+        @test r ≈ 1.0 atol=delta
+        @test abs(v) < epsilon
+        @test it > 0
+    end
+
+    @testset "test tego samego znaku" begin
+        f(x) = x - 1
+        a, b = 2.0, 4.0
+        delta, epsilon = 1e-5, 1e-5
+        r, v, it, err = Solvers.mbisekcji(f, a, b, delta, epsilon)
+        
+        @test err == 1
+        @test r === Nothing
+        @test v === Nothing
+        @test it === Nothing
+    end
+
+    @testset "test sinusa" begin
+        f(x) = sin(x)
+        a, b = 3.0, 4.0
+        delta, epsilon = 1e-5, 1e-5
+        r, v, it, err = Solvers.mbisekcji(f, a, b, delta, epsilon)
+
+        @test err == 0
+        @test r ≈ π atol=delta
+        @test abs(v) < epsilon
+        @test it > 0
+    end
+end
+
+@testset "Testy metody stycznych" begin
+    @testset "Test funkcji kwadratowej" begin
+        f(x) = x^2 - 4
+        pf(x) = 2x
+        x0 = 1.0
+        delta, epsilon = 1e-5, 1e-5
+        maxit = 100
+        
+        x, y, it, err = Solvers.mstycznych(f, pf, x0, delta, epsilon, maxit)
+        
+        @test err == 0
+        @test x ≈ sqrt(4) atol=delta
+        @test abs(y) < epsilon
+        @test it > 0
+    end
+
+    @testset "Test dla funkcji sześciennej" begin
+        f(x) = x^3 - 1
+        pf(x) = 3x^2
+        x0 = 0.5
+        delta, epsilon = 1e-5, 1e-5
+        maxit = 100
+        
+        x, y, it, err = Solvers.mstycznych(f, pf, x0, delta, epsilon, maxit)
+        
+        @test err == 0
+        @test x ≈ 1.0 atol=delta
+        @test abs(y) < epsilon
+        @test it > 0
+    end
+
+    @testset "Test zbyt małej ilości iteracji" begin
+        f(x) = cos(x)
+        pf(x) = -sin(x)
+        x0 = 1.0
+        delta, epsilon = 1e-5, 1e-5
+        maxit = 2
+        
+        x, y, it, err = Solvers.mstycznych(f, pf, x0, delta, epsilon, maxit)
+
+        @test it == maxit
+        @test err == 1
+    end
+end
+
+
+@testset "Testy metody siecznych" begin
+
+    @testset "Test dla funkcji kwadratowej" begin
+        f(x) = x^2 - 4
+        x0, x1 = 1.0, 2.0
+        delta, epsilon = 1e-5, 1e-5
+        maxit = 100
+
+        x, y, it, err = Solvers.msiecznych(f, x0, x1, delta, epsilon, maxit)
+
+        @test err == 0
+        @test x ≈ sqrt(4) atol=delta
+        @test abs(y) < epsilon
+        @test it < maxit
+    end
+
+    @testset "Test dla funkcji sześciennej" begin
+        f(x) = x^3 - 1
+        x0, x1 = 0.0, 2.0
+        delta, epsilon = 1e-5, 1e-5
+        maxit = 100
+
+        x, y, it, err = Solvers.msiecznych(f, x0, x1, delta, epsilon, maxit)
+
+        @test err == 0
+        @test x ≈ 1.0 atol=delta
+        @test abs(y) < epsilon
+        @test it < maxit
+    end
+
+    @testset "Test zbyt małej ilości iteracji" begin
+        f(x) = cos(x) - x
+        x0, x1 = 0.0, 1.0
+        delta, epsilon = 1e-5, 1e-5
+        maxit = 2
+
+        x, y, it, err = Solvers.msiecznych(f, x0, x1, delta, epsilon, maxit)
+
+        @test it == maxit
+        @test err == 1
+    end
+
+end
